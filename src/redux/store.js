529e42d6e05adda0b9f8301316460fcee70f8687
RinaOcean <antyfifa@gmail.com>
2021-05-15 23:19:55 +0300

    add middleware in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 
 import contactsReducer from './contacts/contacts-reducer';
+
+const middleware = [...getDefaultMiddleware(), logger];
 
 const store = configureStore({
   reducer: {
     contacts: contactsReducer,
   },
+  middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
 
